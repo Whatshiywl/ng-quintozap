fb973463d3b0df3a917fa8dc61002b335ff71fb6
Whatshiywl <whatshiywl@gmail.com>
2021-07-11 19:04:49 -0300

    Clean-up gitlab-ci

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,6 @@
-#! /bin/bash
+#! /bin/sh
 set -e
 
-if [ -f .env ]; then
-  source .env
-fi
-
-tVersion=$(node get-version.js)
-pVersion=${tVersion:-1.0}
-read -p "Container version $pVersion? " uVersion
-version=${uVersion:-$pVersion}
+gcloud auth activate-service-account --project $GCP_PROJECT --key-file $GCP_SERVICE_KEY
 gcloud config set project $GCP_PROJECT
-image=gcr.io/$GCP_PROJECT/$GCP_SERVICE:$version
-gcloud builds submit --tag $image
-gcloud run deploy $GCP_SERVICE --image $image --region $GCP_REGION --platform managed --quiet --allow-unauthenticated --concurrency 5 --labels stage=prod
+gcloud run deploy $GCP_SERVICE --service-account $GCP_SERVICE_ACCOUNT --image $image --region $GCP_REGION --platform managed --quiet --allow-unauthenticated --concurrency 5 --labels stage=prod
--- get-version.sh
+++ get-version.sh
@@ -1,3 +1,4 @@
 #! /bin/sh
+set -e
 
 cat package.json | grep version | sed -e 's#\s*"version":\s*"\([^"]*\)",\?\s*#\1#g'
