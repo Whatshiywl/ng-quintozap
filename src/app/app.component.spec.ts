00409b38c073184efab63636654c6903b4c03622
Whatshiywl <whatshiywl@gmail.com>
2021-07-12 21:05:12 -0300

    Aff missing testbed config

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,4 +1,9 @@
 import { TestBed } from '@angular/core/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSliderModule } from '@angular/material/slider';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
@@ -7,19 +12,27 @@ import { ZapService } from './zap.service';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
+    await TestBed
+    .configureTestingModule({
       declarations: [
         AppComponent
       ],
       imports: [
         BrowserModule,
         BrowserAnimationsModule,
-        MapModule
+        FormsModule,
+        ReactiveFormsModule,
+        MapModule,
+        MatSidenavModule,
+        MatInputModule,
+        MatFormFieldModule,
+        MatSliderModule
       ],
       providers: [
         ZapService
       ]
-    }).compileComponents();
+    })
+    .compileComponents();
   });
 
   it('should create the app', () => {
